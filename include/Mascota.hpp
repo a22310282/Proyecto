2f907a04b93a3caaf80ab8afe6c3dfb7d9b979d0
Marco César Jiménez De La Torre <143144886+a22310282@users.noreply.github.com>
2023-09-08 01:55:01 +0000

    Resultados de la clase 07/09/2023

---
+++ include/Mascota.hpp
@@ -0,0 +1,32 @@
+#include <string>
+
+class Mascota
+{
+private:
+    int hambre;
+    std::string nombre;
+
+public:
+    Mascota()
+    {
+        this->hambre = 0;
+        this->nombre = nombre;
+    }
+    ~Mascota() {}
+    void Jugar()
+    {
+        this->hambre += 5;
+    }
+    void Comer(int comida)
+    {
+        this->hambre -= comida;
+    }
+    std::string DecirNombre()
+    {
+        return this->nombre;
+    }
+    int DecirHombre()
+    {
+        return this->hambre;
+    }
+};
---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <Mascota.hpp>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "Juego de mascotas" << std::endl;
+    Mascota m1("Firulais");
+    m1.Jugar();
+    m1.Jugar();
+    m1.Jugar();
+    
+    std::cout
+    << m1.DecirNombre() << "Tiene"
+    << m1.DecirHambre() << "de hambre" << std :: endl;
+
+    m1.Comer(5);
+
+    return 0;
+};
